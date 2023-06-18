3b20c8ca64fbcf6c43c27bdb92cf7928e22ae9aa
kasemsan00 <kasemsan.cho@gmail.com>
2023-06-18 12:28:06 +0700

    dev

--- src/ui/Content/Welcome.tsx
+++ src/ui/Content/Welcome.tsx
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
+
+const line1 = "Hello";
+const line2 = "My name is Kasemsan Chompuwised";
+const line3 = "I'm Fullstack WebDeveloper";
 
 const Welcome = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const line1Ref = useRef<HTMLDivElement>(null);
+  const line2Ref = useRef<HTMLDivElement>(null);
+  const line3Ref = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    const typeWriter = (text: string, ref: React.RefObject<any>) => {
+      let i = 0;
+      const interval = setInterval(() => {
+        ref.current.innerHTML += text.charAt(i++);
+        console.log("Text");
+        if (i === text.length) {
+          clearInterval(interval);
+        }
+      }, 100);
+    };
+    typeWriter(line1, line1Ref);
+    typeWriter(line2, line2Ref);
+    typeWriter(line3, line3Ref);
+  }, []);
+
   return (
     <div ref={ref} className="flex flex-row">
       <div className="flex flex-col gap-6 w-3/4">
         <div
-          className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white
-        pr-5 text-5xl text-black font-bold"
-        >
-          Hello
-        </div>
+          ref={line1Ref}
+          className="overflow-hidden whitespace-nowrap border-r-4 border-r-white
+        pr-5 h-[50px] text-5xl text-black font-bold"
+        ></div>
         <div
-          className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white
-        pr-5 text-5xl text-black font-bold"
-        >
-          My name is Kasemsan Chompuwised
-        </div>
+          ref={line2Ref}
+          className="overflow-hidden whitespace-nowrap border-r-4 border-r-white
+        pr-5 h-[50px] text-5xl text-black font-bold"
+        ></div>
         <div
-          className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white
-        pr-5 text-5xl text-black font-bold"
-        >
-          I&apos;m Fullstack WebDeveloper
-        </div>
+          ref={line3Ref}
+          className="overflow-hidden whitespace-nowrap border-r-4 border-r-white
+        pr-5 h-[50px] text-5xl text-black font-bold"
+        ></div>
       </div>
       <div className="w-fit">Some thing </div>
     </div>
